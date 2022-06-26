1c2c7529ba1790a60442469652b5cf9ac7f078e7
Harry Marshall <harrymarshall@protonmail.com>
2022-06-26 11:12:42 +1000

    selectors made

---
+++ js/cupcakeDropdown.js
@@ -0,0 +1,8 @@
+function cupcakeDropdown() {
+    var x = document.getElementById("cupcake-drop");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
